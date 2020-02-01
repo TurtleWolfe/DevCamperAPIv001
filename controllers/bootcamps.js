136469d9bef00ffc4f816f00572bdace33a0154c
TurtleWolfe <JonPohlner@gmail.com>
2020-01-31 20:10:44 -0500

    end of section 4, episode 24

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,44 +1,163 @@
+const BootCamp = require('../models/BootCamp');
+
 // @desc    Create a new BootCamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootCamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Creat new BootCamp` });
+exports.createBootCamp = async (req, res, next) => {
+
+    try {
+        const bootCamp = await BootCamp.create(req.body);
+        // console.log(req.body);
+        res
+            .status(201)
+            .json({
+                success: true,
+                msg: `Created a new BootCamp`,
+                data: bootCamp,
+            });
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+                success: false,
+                msg: `are you sure that name is unique?`,
+                // data: bootCamp,
+            });
+    }
+
 };
 
 // @desc    Read all BootCamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootCamps = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: 'Show all BootCamps' });
+exports.getBootCamps = async (req, res, next) => {
+    try {
+        const bootCamps = await BootCamp.find();
+        if (!bootCamps) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    msg: `GET BootCamp by ID ${req.params.id} is an odd request, ain't it?`,
+                    // data: bootCamp,
+                });
+        }
+        res
+            .status(200)
+            .json({
+                success: true,
+                count: bootCamps.length,
+                msg: `GET all ${bootCamps.length} BootCamps`,
+                data: bootCamps,
+            });
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+                success: false,
+                msg: `GET BootCamp by ID ${req.params.id} is oddly formatted for MongoID`,
+                // data: bootCamps,
+            });
+    }
 };
 
 // @desc    Read a single BootCamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootCamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `GET BootCamp ${req.params.id}` });
+exports.getBootCamp = async (req, res, next) => {
+    try {
+        const bootCamp = await BootCamp.findById(req.params.id);
+        // console.log(req.body);
+        if (!bootCamp) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    msg: `GET BootCamp by ID ${req.params.id} is not found in this database`,
+                    // data: bootCamp,
+                });
+        }
+        res
+            .status(200)
+            .json({
+                success: true,
+                msg: `GET BootCamp by ID ${req.params.id} out of ${bootCamps.length}`,
+                data: bootCamp,
+            });
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+                success: false,
+                msg: `GET BootCamp by ID ${req.params.id} is an odd request..`,
+                // data: bootCamp,
+            });
+    }
 };
 
 // @desc    Update a single BootCamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootCamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Update BootCamp ${req.params.id}` });
+exports.updateBootCamp = async (req, res, next) => {
+    try {
+        const bootCamp = await BootCamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!bootCamp) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    msg: `PUT BootCamp by ID ${req.params.id} is not found in this database`,
+                });
+        }
+        res
+            .status(200)
+            .json({
+                success: true,
+                msg: `Update BootCamp ${req.params.id}`,
+                data: bootCamp
+            });
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+                success: false,
+                msg: `PUT BootCamp by ID ${req.params.id} is an ODDITY not found in this database`,
+            });
+    }
 };
 
 // @desc Delete a single BootCamp
 // @route Get /api/v1/bootcamp/:id
 // @access Private
-exports.deleteBootCamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Delete BootCamp ${req.params.id}` });
+exports.deleteBootCamp = async (req, res, next) => {
+    try {
+        const bootCamp = await BootCamp.findByIdAndDelete(req.params.id);
+
+        if (!bootCamp) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    msg: `DELETE BootCamp by ID ${req.params.id} is not found in this database`,
+                });
+        }
+        res
+            .status(200)
+            .json({
+                success: true,
+                msg: `Deleted BootCamp by ID ${req.params.id}`,
+                data: bootCamp
+            });
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+                success: false,
+                msg: `DELETE BootCamp by ID ${req.params.id} is an ODDITY not found in this database`,
+            });
+    }
 };
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-//Route Files
-const bootcamps = require('./routes/bootcamps');
-
-//Load env vars
+//  Load env vars
 dotenv.config({ path: './config/config.env' });
+
+//  Connect to the database
+connectDB();
+
+//  Route Files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-// Dev logging middleware
+//  Body Parser
+app.use(express.json());
+
+//  Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Mount Routers
+//  Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
 // const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server listening in ${process.env.NODE_ENV} on port ${PORT}!`)
-);
+    console.log(`Server listening in ${process.env.NODE_ENV} on port ${PORT}!`.yellow.bold));
+
+//  Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
